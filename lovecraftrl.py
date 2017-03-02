92660d6dfc31198c920436bf497fb605618ed648
Brian O'Connor <brn.oconnor32@gmail.com>
2017-03-01 20:00:11 -0500

    Add binary space partitioning for map gen.

--- lovecraftrl.py
+++ lovecraftrl.py
@@ -2,6 +2,7 @@ import libtcodpy as libtcod
 import math
 import textwrap
 import shelve
+import random
 
 # Last change: Reworked random monster/item selection
 
@@ -84,6 +85,11 @@ color_light_floor = libtcod.white
 # player move and attack delays
 PLAYER_MOVE_DELAY = 1
 PLAYER_ATTACK_DELAY = 1
+
+# binary space partitioning constants
+DEPTH = 6
+MIN_SIZE = 5
+FULL_ROOMS = False
 
 
 
@@ -508,7 +514,7 @@ def new_game():
     # generate the map (but don't draw to screen yet) and
     #  initialize fov
     dungeon_level = 1
-    make_map()
+    make_bsp()
     initialize_fov()
 
     game_state = 'playing'
@@ -929,7 +935,7 @@ def next_level():
     save_floor(dungeon_level)
     dungeon_level += 1
     if dungeon_level > len(floors):
-        make_map()
+        make_bsp()
         message('You descend deeper into the heart of the dungeon...',
                 libtcod.red)
     else:
@@ -1601,6 +1607,185 @@ def get_all_equipped(obj):
     else:
         return []
 
+
+def make_bsp():
+    global map, objects, downstairs, upstairs, bsp_rooms, dungeon_level
+    # make map using binary space partitioning
+
+    map = [[Tile(True) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
+
+    objects = [player]
+
+    # empty global list for storing room coordinates
+    bsp_rooms = []
+
+    # new root node
+    bsp = libtcod.bsp_new_with_size(0, 0, MAP_WIDTH, MAP_HEIGHT)
+
+    # split into nodes
+    libtcod.bsp_split_recursive(bsp, 0, DEPTH, MIN_SIZE + 1, MIN_SIZE + 1,
+                                1.5, 1.5)
+
+    # traverse the nodes and create rooms
+    libtcod.bsp_traverse_inverted_level_order(bsp, traverse_node)
+
+    # random room for the stairs
+    downstairs_room = random.choice(bsp_rooms)
+    bsp_rooms.remove(downstairs_room)
+    downstairs = Object(downstairs_room[0], downstairs_room[1], '>',
+                        'downstairs', libtcod.white, always_visible=True)
+    objects.append(downstairs)
+    downstairs.send_to_back()
+
+    # random room for player start (and upstairs, if not on first floor)
+    player_room = random.choice(bsp_rooms)
+    bsp_rooms.remove(player_room)
+    player.x = player_room[0]
+    player.y = player_room[1]
+    if dungeon_level > 1:
+        upstairs = Object(player_room[0], player_room[1], '<',
+                          'upstairs', libtcod.white, always_visible=True)
+        objects.append(upstairs)
+        upstairs.send_to_back()
+
+    # add monsters and items
+    for room in bsp_rooms:
+        new_room = Rect(room[0], room[1], 2, 2)
+        place_objects(new_room)
+
+    initialize_fov()
+
+
+
+def traverse_node(node, dat):
+    global map, bsp_rooms
+
+    # create rooms
+    if libtcod.bsp_is_leaf(node):
+        minx = node.x + 1
+        maxx = node.x + node.w - 1
+        miny = node.y + 1
+        maxy = node.y + node.h - 1
+
+        if maxx == MAP_WIDTH - 1:
+            maxx -= 1
+        if maxy == MAP_HEIGHT - 1:
+            maxy -= 1
+
+        # if FULL_ROOMS is False room sizes are random, otherwise size of node
+        if FULL_ROOMS == False:
+            minx = libtcod.random_get_int(None, minx, maxx - MIN_SIZE + 1)
+            miny = libtcod.random_get_int(None, miny, maxy - MIN_SIZE + 1)
+            maxx = libtcod.random_get_int(None, minx + MIN_SIZE - 2, maxx)
+            maxy = libtcod.random_get_int(None, miny + MIN_SIZE - 2, maxy)
+
+        node.x = minx
+        node.y = miny
+        node.w = maxx - minx + 1
+        node.h = maxy - miny + 1
+
+        # dig room
+        for x in range(minx, maxx + 1):
+            for y in range(miny, maxy + 1):
+                map[x][y].blocked = False
+                map[x][y].block_sight = False
+
+        bsp_rooms.append(((minx + maxx) / 2, (miny + maxy) / 2))
+
+    # create corridors
+    else:
+        left = libtcod.bsp_left(node)
+        right = libtcod.bsp_right(node)
+        node.x = min(left.x, right.x)
+        node.y = min(left.y, right.y)
+        node.w = max(left.x + left.w, right.x + right.w) - node.x
+        node.h = max(left.y + left.h, right.y + right.h) - node.y
+        if node.horizontal:
+            if left.x + left.w - 1 < right.x or right.x + right.w - 1 < left.x:
+                x1 = libtcod.random_get_int(None, left.x, left.x + left.w - 1)
+                x2 = libtcod.random_get_int(None, right.x,
+                                            right.x + right.w - 1)
+                y = libtcod.random_get_int(None, left.y + left.h, right.y)
+                vline_up(map, x1, y - 1)
+                hline(map, x1, y, x2)
+                vline_down(map, x2, y + 1)
+            else:
+                minx = max(left.x, right.x)
+                maxx = min(left.x + left.w - 1, right.x + right.w - 1)
+                x = libtcod.random_get_int(None, minx, maxx)
+                vline_down(map, x, right.y)
+                vline_up(map, x, right.y - 1)
+        
+        else:
+            if left.y + left.h - 1 < right.y or right.y + right.h - 1 < left.y:
+                y1 = libtcod.random_get_int(None, left.y, left.y + left.h - 1)
+                y2 = libtcod.random_get_int(None, right.y,
+                                            right.y + right.h - 1)
+                x = libtcod.random_get_int(None, left.x + left.w, right.x)
+                hline_left(map, x - 1, y1)
+                vline(map, x, y1, y2)
+                hline_right(map, x + 1, y2)
+            else:
+                miny = max(left.y, right.y)
+                maxy = min(left.y + left.h - 1, right.y + right.h - 1)
+                y = libtcod.random_get_int(None, miny, maxy)
+                hline_left(map, right.x - 1, y)
+                hline_right(map, right.x, y)
+                
+    return True
+
+
+
+def vline(map, x, y1, y2):
+    if y1 > y2:
+        y1, y2 = y2, y1
+
+    for y in range(y1, y2 + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
+
+def vline_up(map, x, y):
+    while y >= 0 and map[x][y].blocked == True:
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        y -= 1
+
+
+
+def vline_down(map, x, y):
+    while y < MAP_HEIGHT and map[x][y].blocked == True:
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        y += 1
+
+
+
+def hline(map, x1, x2, y):
+    if x1 > x2:
+        x1, x2 = x2, x1
+
+    for x in range(x1, x2 + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+
+
+def hline_left(map, x, y):
+    while x >= 0 and map[x][y].blocked == True:
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        x -= 1
+
+
+
+def hline_right(map, x, y):
+    while x <= MAP_WIDTH and map[x][y].blocked == True:
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        x += 1
+    
 
 
 #########################
